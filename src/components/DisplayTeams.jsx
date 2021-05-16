4441aa336be6fd24fbe097b90ab178dc890125ea
brandonnhem <brandonnhem@gmail.com>
2021-05-15 20:42:28 -0700

    added new page on submit

---
+++ src/components/DisplayTeams.jsx
@@ -0,0 +1,27 @@
+function DisplayTeams(props) {
+    const _ = require('lodash');
+
+    let chunkTeams = [];
+    const arr = _.shuffle(props.players);
+    var i,j,temparray,chunk = (arr.length / props.teams);
+    for (i=0,j=arr.length; i<j; i+=chunk) {
+        temparray = arr.slice(i,i+chunk);
+        chunkTeams.push(temparray);
+    }
+
+    return (
+        <div className="card center team-list">
+            <h2>Teams</h2>
+            {chunkTeams.map((value, index) => {
+                return <div key={ index } className="card">
+                    Team #{index + 1}
+                    {value.map((player, idx) => {
+                        return <div key={idx} className="player-card">{player}</div>
+                    })}
+                </div>
+            })}
+        </div>
+    )
+}
+
+export default DisplayTeams;
